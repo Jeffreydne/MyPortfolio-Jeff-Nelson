e5eb9be2426aada898855e5dc304042912d29df6
Jeffreydne <Jeffreydne@gmail.com>
2024-03-23 14:06:36 -0700

    add HTML for Intro.jsx & MainContent.jsx, and import to activate each

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from "./components/Header";
-
+import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import './App.css'
 
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <Header />
-      {/* <Footer /> */}
+      <MainContent />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/Intro.jsx
@@ -0,0 +1,18 @@
+function Intro() {
+    return (
+        <>
+            <div id="main-head">
+                <h1>Hi, I&lsquo;m Jeff Nelson</h1>
+                <h2>Let&lsquo;s talk about what I can do to help you meet your goals!</h2>
+            </div>
+            <section id="about">
+                <h2 id="projects-scroll">About Me</h2>
+                <p>I am a full stack web developer who can help your company grow your products by launching (or updating) a well designed, compelling website with great Search Engine Optimization.</p>
+                <p>I am available to expertly take care of all your website needs! Whether you are looking to fix bugs, update an aging site, or develop an entirely new project, I am here to help you!</p>
+                <p>Prior to becoming a web developer, I spent 30 years in healthcare, retiring in early 2020. Outside of work I enjoy boardgames, playing sports and travel.</p>
+            </section>
+        </>
+    )
+
+}
+export default Intro;
