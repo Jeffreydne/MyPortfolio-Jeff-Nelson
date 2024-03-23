1f9c4f57f9f923e8a0b8d0d16f3b50e759154f3c
Jeffreydne <Jeffreydne@gmail.com>
2024-03-23 14:21:06 -0700

    debug Projects.jsx

---
+++ src/components/Projects.jsx
@@ -0,0 +1,10 @@
+function Projects() {
+    return (
+        <>
+            <p>Placeholder for projects</p>
+            <p> Is this a problem</p>
+        
+        </>
+    )
+}
+export default Projects;
