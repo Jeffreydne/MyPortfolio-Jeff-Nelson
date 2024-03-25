00699ada41504b197e7f98e0f7277c0432787847
Jeffreydne <Jeffreydne@gmail.com>
2024-03-25 15:17:59 -0700

    minor changes to contact form and CSS for this form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,14 +5,16 @@ function ContactForm() {
                 <form>
                     <fieldset>
                         <legend>Send your contact info and a comment to me</legend>
-                        <input type="text" name="name" size ="40" id="name" placeholder="enter your name">
+                        <input type="text" name="name" size ="40" id="name" required placeholder="enter your name">
 
                         </input>
-                        <input name="email" type="email" id="email" size="40" placeholder="enter your email">
+                        <input name="email" type="email" id="email" size="40" required placeholder="enter your email">
 
                         </input>
-                        <textarea name="comments" cols="40" rows="2" placeholder="enter a comment"></textarea>
-                        <button id="btn" type="submit">Submit</button>
+                        <div id="formBottom">
+                            <textarea name="comments" cols="40" rows="2" placeholder="enter a comment"></textarea>
+                            <button id="btn" type="submit">Submit</button>
+                        </div>
                     </fieldset>
                 </form>
             </div>
