6f78f983f968c7b60fd0a23d56d431a06d28d837
Jeffreydne <Jeffreydne@gmail.com>
2024-03-25 08:50:54 -0700

    add form functionality and styling

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,14 +3,17 @@ function ContactForm() {
         <>
             <div id="contactFormDiv">
                 <form>
-                    <input type="text" name="name" size ="40" id="name" placeholder="enter your name">
+                    <fieldset>
+                        <legend>Send your contact info and a comment to me</legend>
+                        <input type="text" name="name" size ="40" id="name" placeholder="enter your name">
 
-                    </input>
-                    <input name="email" type="email" id="email" size="40" placeholder="enter your email">
+                        </input>
+                        <input name="email" type="email" id="email" size="40" placeholder="enter your email">
 
-                    </input>
-                    <textarea name="comments" cols="40" rows="2" placeholder="enter a comment"></textarea>
-                    <button type="submit">Submit</button>
+                        </input>
+                        <textarea name="comments" cols="40" rows="2" placeholder="enter a comment"></textarea>
+                        <button id="btn" type="submit">Submit</button>
+                    </fieldset>
                 </form>
             </div>
         </>
